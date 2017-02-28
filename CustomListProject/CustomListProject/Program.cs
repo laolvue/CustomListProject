c35eeb53b32e6a9577951c6b4e26ebf678f394f0
Lao Vue <Lao Vue>
2017-02-28 17:48:58 -0600

    added ADD and SUBTRACT methods

---
+++ CustomListProject/CustomListProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetList<string> setList = new SetList<string>();
+            setList.AddList("bob");
+            setList.AddList("Lao");
+            setList.AddList("car");
+            setList.SubtractList();
+            setList.AddList("duo");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CustomListProject/CustomListProject/SetList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListProject
+{
+    public class SetList<T>
+    {
+        public T[] test;
+        public int counter = 0;
+        public SetList()
+        {
+            test = new T[1];
+        }
+
+        public void AddList(T value)
+        {
+            counter++;
+            T[] temporaryList = test;
+            test = new T[counter];
+            if(counter == 0)
+            {
+                for (int i = (counter - 1); i >= 0; i--)
+                {
+                    test[i] = temporaryList[i];
+                }
+            }
+            else
+            {
+                for (int i = (counter - 1); i > 0; i--)
+                {
+                    test[i-1] = temporaryList[i-1];
+                }
+            }
+
+            test[counter-1] = value;
+        }
+
+        public void SubtractList()
+        {
+            counter--;
+            T[] temporaryList = test;
+            test = new T[counter];
+
+            for (int i = 0; i < (counter); i++)
+            {
+                test[i] = temporaryList[i];
+            }
+        }
+
+
+    }
+}
